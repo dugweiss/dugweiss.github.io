a43634e23d29843b8ede5b00840253930294e0ae
Douglas Weiss <douglasweiss@Douglass-MacBook-Pro.local>
2020-03-28 16:53:37 -0400

    Add alert

--- scripts/script.js
+++ scripts/script.js
@@ -2,6 +2,7 @@ function printMessage() {
     var msg = "Hello Doug";
     msg += "!"
     console.log(msg);
+    alert(msg)
 }
 function getData() {
     document.getElementById("text-location").innerHTML = "hi there";    
