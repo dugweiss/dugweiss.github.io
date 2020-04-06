dc7f8407bb416841e070db700a631a7d7f44f11e
Douglas Weiss <douglasweiss@Douglass-MacBook-Pro.local>
2020-04-06 15:40:20 -0400

    Add Page 5 Add logTextToConsole Add 2 squares

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,36 @@
 function printMessage() {
     var msg = "Hello Doug";
-    msg += "!"
+    msg += "!";
     console.log(msg);
-    alert(msg)
+    alert(msg);
 }
 function getData() {
     document.getElementById("text-location").innerHTML = "hi there";    
 }
+function logTxtToConsole() {
+    var msg = "working";
+    console.log(msg);
+    var msg2 = document.getElementById("usernamefield").value;
+    console.log(msg2);
+}
+
+class Square {
+    constructor(length) {
+        this.edgeLength = length;
+    }
+    getLength(){
+        console.log(this.edgeLength);
+    }
+    getArea(){
+        var area = this.edgeLength*this.edgeLength
+        console.log(area)
+    }
+}
+var mySquarea = new Square(5);
+var mySquareb = new Square(10);
+
+mySquarea.getLength();
+mySquarea.getArea();
+
+mySquareb.getLength();
+mySquareb.getArea();
